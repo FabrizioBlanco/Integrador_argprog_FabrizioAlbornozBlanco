a4f02a60bfc7de7dc01efa5850a25d5dd554dc39
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2022-09-05 11:59:54 -0300

    componente de redes sociales

--- Integrador_arg_FabrizioAlbornozBlanco/src/app/app.module.ts
+++ Integrador_arg_FabrizioAlbornozBlanco/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogosComponent } from './components/logos/logos.component';
+import { SocialesComponent } from './components/sociales/sociales.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogosComponent
+    LogosComponent,
+    SocialesComponent
   ],
   imports: [
     BrowserModule,

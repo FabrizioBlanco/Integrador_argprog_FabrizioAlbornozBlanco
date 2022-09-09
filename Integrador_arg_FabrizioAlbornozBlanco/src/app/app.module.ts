f1e48f79aa86c5443a5480d9a2fae236f0357d78
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2022-09-09 19:22:05 -0300

    agregando modulo de experiencia

--- Integrador_arg_FabrizioAlbornozBlanco/src/app/app.module.ts
+++ Integrador_arg_FabrizioAlbornozBlanco/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LogosComponent } from './components/logos/logos.component';
 import { SocialesComponent } from './components/sociales/sociales.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogosComponent,
     SocialesComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,

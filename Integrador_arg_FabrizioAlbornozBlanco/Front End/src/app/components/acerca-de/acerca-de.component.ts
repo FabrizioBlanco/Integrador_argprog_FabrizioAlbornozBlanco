444d79445bf45955e713997e29b74a8ef339dace
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2022-09-20 16:02:41 -0300

    update component

--- Integrador_arg_FabrizioAlbornozBlanco/Front End/src/app/components/acerca-de/acerca-de.component.ts
+++ Integrador_arg_FabrizioAlbornozBlanco/Front End/src/app/components/acerca-de/acerca-de.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { persona } from 'src/app/model/Persona.model';
+import { persona } from 'src/app/model/persona.model';
 import { PersonaService } from 'src/app/service/persona.service';
+
 
 @Component({
   selector: 'app-acerca-de',
@@ -8,12 +9,18 @@ import { PersonaService } from 'src/app/service/persona.service';
   styleUrls: ['./acerca-de.component.css']
 })
 export class AcercaDeComponent implements OnInit {
-  persona: persona = new persona("","","")
-  constructor(public personaService: PersonaService) { }
+  persona: persona = new persona("","","");
+  constructor(public personaService: PersonaService) {}
 
+  //aquí llamamos/usamos a nuestro servicio de persona  
   ngOnInit(): void {
-    //suscribe conecta el observer con los eventos observables. Cuando detecta un cambio, ejecuta el código.
-    this.personaService.getPersona().subscribe(data => {this.persona=data})
+    this.personaService
+      .getPersona()
+      .subscribe(data => { 
+      /*suscribe conecta el observer con los eventos observables.
+      Cuando detecta un cambio, ejecuta el código.*/
+        this.persona=data;
+      })
   }
-
+  
 }

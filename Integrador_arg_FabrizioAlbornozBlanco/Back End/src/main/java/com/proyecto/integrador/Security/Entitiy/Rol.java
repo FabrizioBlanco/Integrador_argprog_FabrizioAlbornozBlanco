bbc2b4e64b5cacd912be260df5756a1855035422
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2022-09-26 12:20:16 -0300

    Clase Rol terminada

--- Integrador_arg_FabrizioAlbornozBlanco/Back End/src/main/java/com/proyecto/integrador/Security/Entitiy/Rol.java
+++ Integrador_arg_FabrizioAlbornozBlanco/Back End/src/main/java/com/proyecto/integrador/Security/Entitiy/Rol.java
@@ -1,14 +1,46 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.proyecto.integrador.Security.Entitiy;
 
-/**
- *
- * @author fabri
- */
+import com.proyecto.integrador.Security.Enums.RolNombre;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
+
 public class Rol {
+    @Id
+    @GeneratedValue(strategy =GenerationType.IDENTITY )
+    private int id;
+    
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RolNombre rolNombre;
+
+    public Rol() {
+    }
+
+    public Rol(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public RolNombre getRolNombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(RolNombre rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+    
     
 }

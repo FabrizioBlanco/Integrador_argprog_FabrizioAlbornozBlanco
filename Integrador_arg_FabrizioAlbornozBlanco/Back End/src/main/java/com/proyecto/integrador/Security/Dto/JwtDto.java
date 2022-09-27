0a71c231d2e7060560223d9b658e08f216e4b31f
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2022-09-26 21:09:09 -0300

    Dto terminado

--- Integrador_arg_FabrizioAlbornozBlanco/Back End/src/main/java/com/proyecto/integrador/Security/Dto/JwtDto.java
+++ Integrador_arg_FabrizioAlbornozBlanco/Back End/src/main/java/com/proyecto/integrador/Security/Dto/JwtDto.java
@@ -5,10 +5,53 @@
  */
 package com.proyecto.integrador.Security.Dto;
 
-/**
- *
- * @author fabri
- */
-public class JwtDto {
+//cuando hagamos un login nos devuelve un JWT
+
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+
+public class JwtDto { //DTO: Data Transfer Object
+    private String token;
+    private String bearer = "Bearer";
+    private String nombreUsuario;
+    private Collection <? extends GrantedAuthority> authorities;
+
+    public JwtDto(String token, String nombreUsuario, Collection<? extends GrantedAuthority> authorities) {
+        this.token = token;
+        this.nombreUsuario = nombreUsuario;
+        this.authorities = authorities;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getBearer() {
+        return bearer;
+    }
+
+    public void setBearer(String bearer) {
+        this.bearer = bearer;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
     
 }

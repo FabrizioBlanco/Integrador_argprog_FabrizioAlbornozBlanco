bad9c6ddb9ec6689c814d02bd32367ac2569719d
FabrizioBlanco <Fabrizio.blanco2021@gmail.com>
2022-10-08 19:25:35 -0300

    Conectando Front con BD

---
+++ Integrador_arg_FabrizioAlbornozBlanco/Front End/src/app/model/nuevo-usuario.ts
@@ -0,0 +1,8 @@
+export class NuevoUsuario {
+    //Le pongo el ! para que ignore el error de que no están inicializados
+    nombre!:string;
+    nombreUsuario!:string;
+    email!:string;
+    password!:string;
+    Authorities!:string[];
+}
